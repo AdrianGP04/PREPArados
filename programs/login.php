3f0415e620834fcaa0af933b75b0590430b251f3
Adrian Garcia <adriangpprp@outlook.es>
2016-05-23 23:15:06 -0500

    Agregado el sistema de incio se sesión

--- programs/login.php
+++ programs/login.php
@@ -1,3 +1,27 @@
 <?php
-    echo "Página de login";
+    if((isset($_POST["numCuentaLogIn"]) && isset($_POST["passwordLogIn"])) && (!empty($_POST["numCuentaLogIn"]) && !empty($_POST["passwordLogIn"]))){
+        include("conexion.php");
+        $con = mysqli_connect($dbHost,$dbUser,$dbPassword,"PREPArados") or die("Problemas con el servidor");
+        mysqli_select_db($con,"PREPArados") or die ("Problemas al conectar la base de datos");
+        $nocta = mysqli_real_escape_string($con, $_POST["numCuentaLogIn"]);
+        $password = mysqli_real_escape_string($con, $_POST["passwordLogIn"]);
+        mysqli_query ($con, "SET NAMES 'utf8'");
+        $result = mysqli_query($con,"SELECT * FROM USUARIOS WHERE USER_NOCT ='$nocta'");
+		$extraido = mysqli_fetch_assoc($result);
+        if(isset($extraido["USER_NOCT"])){
+            $cpass = substr($extraido["USER_PASS"], 5, 40);
+            if(sha1($password) == $cpass){
+                session_start();
+                $_SESSION["ID"] = $extraido["USER_NOCT"];
+                $_SESSION["name"] = $extraido["USER_NAME"];
+                header("Location: ../perfil/estudiante.html");
+            }
+            else
+                echo "La contraseña es incorrecta";
+        }
+        else
+            echo "Debes registrarte para continuar";
+    }
+    else
+        echo "Debes llenar todos los campos";
 ?>
