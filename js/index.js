476a6f32ff2c3114f484c56c25276352ac9e8b9b
Adrian Garcia <adriangpprp@outlook.es>
2016-05-24 01:13:36 -0500

    Agregada la opción de mostrar contraseñas

--- js/index.js
+++ js/index.js
@@ -1,8 +1,14 @@
 $(document).ready(function(){
+    $(".help").mouseenter(function(){
+        $(".password").attr("type", "text");
+    });
+    $(".help").mouseout(function(){
+        $(".password").attr("type", "password");
+    });
+    $("#numCuentaLogIn").popover({title: "Número de cuenta incorrecto", content: "El número de cuenta ingresado es erróneo", placement: "right"});
+    $("#passwordLogIn").popover({title: "Contraseña incorrecta", content: "La contraseña ingresada es errónea", placement: "right"});
+    $("#modal-header-LogIn").popover({title: "Campos vacios", content: "Debes llenar todos los campos", placement: "right"});
     $("#logInSubmit").click(function(event){
-        $("#numCuentaLogIn").popover({title: "Número de cuenta incorrecto", content: "El número de cuenta ingresado es erróneo", placement: "right"});
-        $("#passwordLogIn").popover({title: "Contraseña incorrecta", content: "La contraseña ingresada es errónea", placement: "right"});
-        $("#modal-header").popover({title: "Campos vacios", content: "Debes llenar todos los campos", placement: "right"});
         event.preventDefault();
         $.post("./programs/login.php",
         {
@@ -11,7 +17,7 @@ $(document).ready(function(){
         },
         function(data){
             if(data == "ERROR: CAMPOS")
-                $("#modal-header").popover("toggle");
+                $("#modal-header-LogIn").popover("toggle");
             else if (data == "ERROR: REGISTRO")
                 $("#numCuentaLogIn").popover("toggle");
             else if (data == "ERROR: CONTRASEÑA")
@@ -23,6 +29,6 @@ $(document).ready(function(){
     $("input").focus(function(){
         $("#numCuentaLogIn").popover("hide");
         $("#passwordLogIn").popover("hide");
-        $("#modal-header").popover("hide");
+        $("#modal-header-LogIn").popover("hide");
     });
 });
