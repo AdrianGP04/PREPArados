12ba55dd8332401c0e2f83a1e46ae8b6a1b04a6a
Adrian Garcia <adriangpprp@outlook.es>
2016-05-26 23:46:55 -0500

    Terminado el sistema de consulta de preguntas

--- preparados.sql
+++ preparados.sql
@@ -1,245 +1,223 @@
--- MySQL dump 10.13  Distrib 5.7.12, for Linux (x86_64)
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
 --
--- Host: localhost    Database: PREPArados
--- ------------------------------------------------------
--- Server version	5.7.12-0ubuntu1
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 24-05-2016 a las 05:21:52
+-- Versión del servidor: 10.1.10-MariaDB
+-- Versión de PHP: 7.0.3
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
 
---
--- Table structure for table `Administrador`
---
-
-DROP TABLE IF EXISTS `Administrador`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `Administrador` (
-  `Usr_Administrador` varchar(30) NOT NULL,
-  `Contra_Administrador` varchar(50) DEFAULT NULL,
-  PRIMARY KEY (`Usr_Administrador`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
+CREATE USER PREPArados@localhost IDENTIFIED BY 'PREPArados';
+CREATE DATABASE PREPArados;
+USE PREPArados;
+REVOKE ALL PRIVILEGES ON *.* FROM 'PREPArados'@'localhost'; GRANT ALL PRIVILEGES ON *.* TO 'PREPArados'@'localhost' REQUIRE NONE WITH GRANT OPTION MAX_QUERIES_PER_HOUR 0 MAX_CONNECTIONS_PER_HOUR 0 MAX_UPDATES_PER_HOUR 0 MAX_USER_CONNECTIONS 0;
 
 --
--- Dumping data for table `Administrador`
+-- Base de datos: `preparados`
 --
 
-LOCK TABLES `Administrador` WRITE;
-/*!40000 ALTER TABLE `Administrador` DISABLE KEYS */;
-/*!40000 ALTER TABLE `Administrador` ENABLE KEYS */;
-UNLOCK TABLES;
+-- --------------------------------------------------------
+CREATE TABLE `administrador` (
+    `ADMIN_ID` int(11) NOT NULL,
+    `ADMIN_NAME` varchar(20) COLLATE latin1_spanish_ci NOT NULL,
+    `ADMIN_PASS` varchar(50) COLLATE latin1_spanish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_spanish_ci;
 
---
--- Table structure for table `Alumnos`
---
+INSERT INTO `administrador` (`ADMIN_ID`, `ADMIN_NAME`, `ADMIN_PASS`) VALUES
+(1, 'ENP 6', '324fd71a26f8762c1caf1a345a83f6c846c4b010bf79c97dfe');
 
-DROP TABLE IF EXISTS `Alumnos`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `Alumnos` (
-  `Num_Alumno` int(11) NOT NULL,
-  `Contra_Alumno` varchar(50) DEFAULT NULL,
-  `Usr_Alumno` varchar(50) DEFAULT NULL,
-  `Anio_Materia` varchar(50) DEFAULT NULL,
-  PRIMARY KEY (`Num_Alumno`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
+CREATE TABLE `areas` (
+    `AREA_ID` int(2) NOT NULL,
+    `AREA_NAME` varchar(75) COLLATE latin1_spanish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_spanish_ci;
 
---
--- Dumping data for table `Alumnos`
---
+INSERT INTO `areas` (`AREA_ID`, `AREA_NAME`) VALUES
+(1, 'Ciencias Físico - Matemáticas y de las Ingenierías'),
+(2, 'Ciencias Biológicas y de la Salud'),
+(3, 'Ciencias Sociales'),
+(4, 'Humanidades y Artes');
 
-LOCK TABLES `Alumnos` WRITE;
-/*!40000 ALTER TABLE `Alumnos` DISABLE KEYS */;
-/*!40000 ALTER TABLE `Alumnos` ENABLE KEYS */;
-UNLOCK TABLES;
+CREATE TABLE `colegios` (
+  `COL_ID` int(2) NOT NULL,
+  `COL_NAME` varchar(35) COLLATE latin1_spanish_ci NOT NULL,
+  `AREA_ID` int(2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_spanish_ci;
 
---
--- Table structure for table `Areas`
---
+INSERT INTO `colegios` (`COL_ID`, `COL_NAME`, `AREA_ID`) VALUES
+(1, 'Fisica', 1),
+(2, 'Informatica', 1),
+(3, 'Matematicas', 1),
+(4, 'Biologia', 2),
+(5, 'Educacion Fisica', 2),
+(6, 'Morfologia, Fisiologia y Salud', 2),
+(7, 'Orientacion Educativa', 2),
+(8, 'Psicologia e Higiene Mental', 2),
+(9, 'Quimica', 2),
+(10, 'Ciencias Sociales', 3),
+(11, 'Geografia', 3),
+(12, 'Historia', 3),
+(13, 'Aleman', 4),
+(14, 'Artes Plasticas', 4),
+(15, 'Danza', 4),
+(16, 'Dibujo y Modelado', 4),
+(17, 'Filosofia', 4),
+(18, 'Frances', 4),
+(19, 'Ingles', 4),
+(20, 'Italiano', 4),
+(21, 'Letras Clasicas', 4),
+(22, 'Literatura', 4),
+(23, 'Musica', 4),
+(24, 'Teatro', 4),
+(25, 'Produccion Editorial', 4);
 
-DROP TABLE IF EXISTS `Areas`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `Areas` (
-  `Num_Area` int(11) NOT NULL,
-  `Nom_Area` varchar(30) DEFAULT NULL,
-  PRIMARY KEY (`Num_Area`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
+CREATE TABLE `materias` (
+  `MAT_ID` int(5) NOT NULL,
+  `MAT_NAME` varchar(75) COLLATE latin1_spanish_ci NOT NULL,
+  `MAT_COL` int(2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_spanish_ci;
 
---
--- Dumping data for table `Areas`
---
+INSERT INTO `materias` (`MAT_ID`, `MAT_NAME`, `MAT_COL`) VALUES
+(1400, 'Matematicas IV', 3),
+(1401, 'Fisica III', 1),
+(1402, 'Lengua Española', 22),
+(1403, 'Historia Universal III', 12),
+(1404, 'Lógica', 17),
+(1405, 'Geografia', 11),
+(1406, 'Dibujo II', 16),
+(1407, 'Lengua Extranjera Ingles IV', 19),
+(1408, 'Lengua Extranjera Frances IV', 18),
+(1409, 'Educacion Estetica y Artistica IV', 14),
+(1410, 'Eduacion Fisica IV', 5),
+(1411, 'Orientacion Educativa IV', 7),
+(1412, 'Informatica', 2),
+(1500, 'Matematicas V', 3),
+(1501, 'Quimica III', 9),
+(1502, 'Biologia IV', 4),
+(1503, 'Educacion para la Salud', 6),
+(1504, 'Historia de Mexico II', 12),
+(1505, 'Etimologias Grecolatinas', 21),
+(1506, 'Lengua Extranjera Ingles V', 19),
+(1507, 'Lengua Extranjera Frances V', 18),
+(1508, 'Lengua Extranjera Italiano I', 20),
+(1509, 'Lengua Extranjera Aleman I', 13),
+(1510, 'Lengua Extranjera Ingles I', 19),
+(1511, 'Lengua Extranjera Frances I', 18),
+(1512, 'Etica', 17),
+(1513, 'Educacion Fisica V', 5),
+(1514, 'Educacion Estetica y Artistica V', 14),
+(1515, 'Orientacion Educativa V', 7),
+(1516, 'Literatura Universal', 22),
+(1601, 'Derecho', 10),
+(1602, 'Literatura mexicana e iberoamericana', 22),
+(1603, 'Lengua Extranjera Ingles VI', 19),
+(1604, 'Lengua Extranjera Frances VI', 18),
+(1605, 'Lengua Extranjera Aleman II', 13),
+(1606, 'Lengua Extranjera Italiano II', 20),
+(1607, 'Lengua Extranjera Ingles II', 19),
+(1608, 'Lengua Extranjera Frances II', 18),
+(1609, 'Psicologia', 8),
+(1700, 'Higiene Mental', 8),
+(1701, 'Teatro VI', 24),
+(1702, 'Musica VI', 23),
+(1712, 'Estadistica y Probabilidad', 3),
+(1610, 'Dibujo Constructivo II', 16),
+(1611, 'Fisica IV', 1),
+(1612, 'Quimica IV', 9),
+(1613, 'Biologia IV', 4),
+(1706, 'Geologia y Mineralogia', 9),
+(1709, 'Fisico-Quimica', 1),
+(1710, 'Temas Selectos de Matematicas', 3),
+(1719, 'Informatica Aplicada a la Ciencia y a la Industria', 2),
+(1721, 'Cosmografia', 11),
+(1613, 'Biologia V', 4),
+(1711, 'Temas Selectos de Biologia', 4),
+(1716, 'Temas Selectos de Morfologia y Fisiologia', 6),
+(1614, 'Geografia Economica', 11),
+(1615, 'Introduccion al Estudio de las Ciencias Sociales y Economicas', 10),
+(1616, 'Problemas Socio Politicos y Economicos de Mexico', 10),
+(1704, 'Contabilidad y Gestion Administrativa', 10),
+(1707, 'Geografia Politica', 11),
+(1720, 'Sociologia', 10),
+(1617, 'Historia de la Cultura', 12),
+(1618, 'Historia de las Doctrinas Filosoficas', 17),
+(1703, 'Revolucion Mexicana', 12),
+(1705, 'Pensamiento Filosofico de Mexico', 17),
+(1708, 'Modelado II', 16),
+(1713, 'Latin', 21),
+(1714, 'Griego', 21),
+(1715, 'Comunicacion Visual', 16),
+(1717, 'Estetica', 17),
+(1600, 'Matematicas VI (Area I y II)', 3),
+(1619, 'Matematicas VI (Area III)', 3),
+(1620, 'Matematicas VI (Area IV)', 3);
 
-LOCK TABLES `Areas` WRITE;
-/*!40000 ALTER TABLE `Areas` DISABLE KEYS */;
-/*!40000 ALTER TABLE `Areas` ENABLE KEYS */;
-UNLOCK TABLES;
+CREATE TABLE `profesor` (
+  `PROF_ID` int(11) NOT NULL,
+  `PROF_NAME` varchar(30) COLLATE latin1_spanish_ci NOT NULL,
+  `PROF_PASS` varchar(50) COLLATE latin1_spanish_ci NOT NULL,
+  `PROF_AREA` int(2) NOT NULL,
+  `PROF_COL` int(2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_spanish_ci;
 
---
--- Table structure for table `Colegios`
---
+CREATE TABLE `usuarios` (
+  `USER_NAME` varchar(40) COLLATE latin1_spanish_ci NOT NULL,
+  `USER_NOCT` char(9) COLLATE latin1_spanish_ci NOT NULL,
+  `USER_PASS` varchar(50) COLLATE latin1_spanish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_spanish_ci;
 
-DROP TABLE IF EXISTS `Colegios`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `Colegios` (
-  `Num_Colegio` int(11) NOT NULL,
-  `Nom_Colegio` varchar(50) DEFAULT NULL,
-  `Nom_Coordinador` varchar(50) DEFAULT NULL,
-  `Contra_Coordinador` varchar(50) DEFAULT NULL,
-  `Num_Area` int(11) DEFAULT NULL,
-  PRIMARY KEY (`Num_Colegio`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `Colegios`
---
+CREATE TABLE `coordinador` (
+  `COORD_ID` int(11) NOT NULL,
+  `COORD_NAME` varchar(30) COLLATE latin1_spanish_ci NOT NULL,
+  `COORD_PASS` varchar(50) COLLATE latin1_spanish_ci NOT NULL,
+  `COORD_AREA` int(2) NOT NULL,
+  `COORD_COL` int(2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_spanish_ci;
 
-LOCK TABLES `Colegios` WRITE;
-/*!40000 ALTER TABLE `Colegios` DISABLE KEYS */;
-/*!40000 ALTER TABLE `Colegios` ENABLE KEYS */;
-UNLOCK TABLES;
+CREATE TABLE `pregunta` (
+  `PREG_ID` int(11) NOT NULL,
+  `PREG_MAT` int(2) NOT NULL,
+  `PREG_TEXT` varchar(200) COLLATE latin1_spanish_ci NOT NULL,
+  `PREG_CORR` varchar(30) COLLATE latin1_spanish_ci NOT NULL,
+  `PREG_INCUNO` varchar(30) COLLATE latin1_spanish_ci NOT NULL,
+  `PREG_INCDOS` varchar(30) COLLATE latin1_spanish_ci NOT NULL,
+  `PREG_INCTRES` varchar(30) COLLATE latin1_spanish_ci NOT NULL,
+  `PREG_PROF` int(11) NOT NULL,
+  `PREG_DATE` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_spanish_ci;
 
---
--- Table structure for table `Materias`
---
+ALTER TABLE `administrador`
+  ADD PRIMARY KEY (`ADMIN_ID`);
 
-DROP TABLE IF EXISTS `Materias`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `Materias` (
-  `Num_Materia` int(11) NOT NULL,
-  `Nom_Materia` varchar(50) DEFAULT NULL,
-  `Anio_Materia` varchar(50) DEFAULT NULL,
-  `Num_Colegio` varchar(50) DEFAULT NULL,
-  PRIMARY KEY (`Num_Materia`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
+ ALTER TABLE `areas`
+  ADD PRIMARY KEY (`AREA_ID`);
 
---
--- Dumping data for table `Materias`
---
+ALTER TABLE `colegios`
+  ADD PRIMARY KEY (`COL_ID`);
 
-LOCK TABLES `Materias` WRITE;
-/*!40000 ALTER TABLE `Materias` DISABLE KEYS */;
-/*!40000 ALTER TABLE `Materias` ENABLE KEYS */;
-UNLOCK TABLES;
+ALTER TABLE `coordinador`
+  ADD PRIMARY KEY (`COORD_ID`);
 
---
--- Table structure for table `Preguntas`
---
+ALTER TABLE `profesor`
+  ADD PRIMARY KEY (`PROF_ID`);
 
-DROP TABLE IF EXISTS `Preguntas`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `Preguntas` (
-  `Num_Pregunta` int(11) NOT NULL,
-  `Num_Profesor` varchar(50) DEFAULT NULL,
-  `Preg_Pregunta` varchar(100) DEFAULT NULL,
-  `Num_Materia` int(11) DEFAULT NULL,
-  PRIMARY KEY (`Num_Pregunta`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`USER_NOCT`);
 
---
--- Dumping data for table `Preguntas`
---
+ALTER TABLE `pregunta`
+  ADD PRIMARY KEY (`PREG_ID`);
 
-LOCK TABLES `Preguntas` WRITE;
-/*!40000 ALTER TABLE `Preguntas` DISABLE KEYS */;
-/*!40000 ALTER TABLE `Preguntas` ENABLE KEYS */;
-UNLOCK TABLES;
+ALTER TABLE `colegios`
+  MODIFY `COL_ID` int(2) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
 
---
--- Table structure for table `Profesores`
---
+ALTER TABLE `coordinador`
+  MODIFY `COORD_ID` int(11) NOT NULL AUTO_INCREMENT;
 
-DROP TABLE IF EXISTS `Profesores`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `Profesores` (
-  `Num_Profesor` int(11) DEFAULT NULL,
-  `Nom_Profesor` varchar(50) DEFAULT NULL,
-  `Contra_Profesor` varchar(50) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
+ALTER TABLE `profesor`
+  MODIFY `PROF_ID` int(11) NOT NULL AUTO_INCREMENT;
 
---
--- Dumping data for table `Profesores`
---
-
-LOCK TABLES `Profesores` WRITE;
-/*!40000 ALTER TABLE `Profesores` DISABLE KEYS */;
-/*!40000 ALTER TABLE `Profesores` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `Profesores_has_Materias`
---
-
-DROP TABLE IF EXISTS `Profesores_has_Materias`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `Profesores_has_Materias` (
-  `Num_Profesor` int(11) DEFAULT NULL,
-  `Num_Materia` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `Profesores_has_Materias`
---
-
-LOCK TABLES `Profesores_has_Materias` WRITE;
-/*!40000 ALTER TABLE `Profesores_has_Materias` DISABLE KEYS */;
-/*!40000 ALTER TABLE `Profesores_has_Materias` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `Respuestas`
---
-
-DROP TABLE IF EXISTS `Respuestas`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `Respuestas` (
-  `Num_Pregunta` int(11) NOT NULL,
-  `RespCorr_Respuesta` varchar(100) DEFAULT NULL,
-  `Resp2_Respuesta` varchar(100) DEFAULT NULL,
-  `Resp3_Respuesta` varchar(100) DEFAULT NULL,
-  `Resp4_Respuesta` varchar(100) DEFAULT NULL,
-  PRIMARY KEY (`Num_Pregunta`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `Respuestas`
---
-
-LOCK TABLES `Respuestas` WRITE;
-/*!40000 ALTER TABLE `Respuestas` DISABLE KEYS */;
-/*!40000 ALTER TABLE `Respuestas` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2016-05-26 22:28:13
+  ALTER TABLE `pregunta`
+    MODIFY `PREG_ID` int(11) NOT NULL AUTO_INCREMENT;
